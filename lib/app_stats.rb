31f5da06351e848a1410717e92d467e3baa02267
Ryan LeViseur <vidkun@vlfservices.com>
2014-10-07 13:42:26 -0600

    switch quote styles and remove unneeded split

--- lib/app_stats.rb
+++ lib/app_stats.rb
@@ -10,10 +10,10 @@ module AppStats
     HAS_MANY_SEARCH = "ack \" has_many \" -c | awk -F \":\" '{print $2,$1}' | grep -v \"0\" #{PIPE_MINUS_COVERAGE} #{PIPE_MINUS_CURRENT_FILE} | sort -rn"
 
     #Sort the Ruby files in your project by LOC
-    SORT_LOC_SEARCH = "find . -iname \"*.rb\" -type f -exec wc -l {} \; | sort -rn"
+    SORT_LOC_SEARCH = 'find . -iname "*.rb" -type f -exec wc -l {} \; | sort -rn'
 
     #Count the lines of Ruby code in your app
-    LOC_COUNT_SEARCH = "find . -iname \"*.rb\" -type f -exec cat {} \; | wc -l"
+    LOC_COUNT_SEARCH = 'find . -iname "*.rb" -type f -exec cat {} \; | wc -l'
 
     def self.get_raw(command)
       `#{command}`
@@ -28,7 +28,7 @@ module AppStats
     end
 
     def self.get_loc_count
-      get_raw(LOC_COUNT_SEARCH).split("\n")
+      get_raw(LOC_COUNT_SEARCH)
     end
   end
 end
